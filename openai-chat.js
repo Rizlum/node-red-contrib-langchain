249245cfa6fbd539129650c10e2441ed0d1f2a9f
Quang-Linh LE <linktohack@gmail.com>
2024-08-14 14:40:03 +0200

    OpenAI: Chat with tools

--- openai-chat.js
+++ openai-chat.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const openai_1 = __importDefault(require("openai"));
+const ajv_1 = __importDefault(require("ajv"));
 const util_1 = require("util");
 const LangchainChatNodeInitializer = (RED) => {
     const evaluateNodeProperty = (0, util_1.promisify)(RED.util.evaluateNodeProperty);
@@ -12,7 +13,7 @@ const LangchainChatNodeInitializer = (RED) => {
         this.config = RED.nodes.getNode(n.config);
         const node = this;
         node.on("input", async (msg, send, done) => {
-            const { url, apiKey } = this.config;
+            const { url, apiKey } = node.config;
             const customOptions = await evaluateNodeProperty(n.customOptions, n.customOptionsType, node, msg);
             const client = new openai_1.default({
                 baseURL: url,
@@ -20,17 +21,101 @@ const LangchainChatNodeInitializer = (RED) => {
                 ...customOptions
             });
             const messages = await evaluateNodeProperty(n.messages, n.messagesType, node, msg);
-            try {
-                const chatCompletion = await client.chat.completions.create({
+            if (n.tools && n.tools.length > 0) {
+                const tools = await Promise.all(n.tools.map(async (it) => {
+                    const url = await evaluateNodeProperty(it.name, it.nameType, node, msg);
+                    const segs = url.split('/');
+                    const name = segs[segs.length - 1];
+                    const description = await evaluateNodeProperty(it.description, it.descriptionType, node, msg);
+                    const schema = await evaluateNodeProperty(it.schema, it.schemaType, node, msg);
+                    return {
+                        type: 'function',
+                        function: {
+                            parse: (args) => {
+                                const data = JSON.parse(args);
+                                const ajv = new ajv_1.default();
+                                const valid = ajv.validate(schema, data);
+                                if (!valid) {
+                                    const error = new Error(ajv.errors.map(it => `${it}`).join('\n'));
+                                    error.name = 'ParsedError';
+                                    throw error;
+                                }
+                                return data;
+                            },
+                            function: async (args) => {
+                                if (name === '__structured_output__') {
+                                    return args;
+                                }
+                                try {
+                                    const response = await fetch(url, {
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify(args)
+                                    });
+                                    const text = await response.text();
+                                    try {
+                                        const json = JSON.parse(text);
+                                        return json;
+                                    }
+                                    catch (error) {
+                                        return text;
+                                    }
+                                }
+                                catch (error) {
+                                    return error;
+                                }
+                            },
+                            name,
+                            description,
+                            parameters: schema,
+                        },
+                    };
+                }));
+                node.log(`runTool: ${(0, util_1.inspect)({
                     model: n.model,
                     messages,
-                });
-                send(chatCompletion.choices[0].message);
-                done();
+                    tools,
+                }, false, 10)})`);
+                try {
+                    const messages1 = [];
+                    const runner = client.beta.chat.completions
+                        .runTools({
+                        model: n.model,
+                        messages,
+                        tools,
+                    })
+                        .on('message', (message) => {
+                        node.log(`message: ${message}`);
+                        messages1.push(message);
+                    });
+                    const finalContent = await runner.finalContent();
+                    msg.payload = finalContent;
+                    msg.messages = messages1;
+                    send(msg);
+                    done();
+                }
+                catch (error) {
+                    node.error(`error ${error}`);
+                    done(error);
+                }
             }
-            catch (error) {
-                node.error(`error ${error}`);
-                done(error);
+            else {
+                try {
+                    const chatCompletion = await client.chat.completions.create({
+                        model: n.model,
+                        messages,
+                    });
+                    msg.payload = chatCompletion.choices[0].message;
+                    msg.messages = [...messages, messages];
+                    send(msg);
+                    done();
+                }
+                catch (error) {
+                    node.error(`error ${error}`);
+                    done(error);
+                }
             }
         });
     }
--- openai-chat.ts
+++ openai-chat.ts
@@ -2,10 +2,12 @@ import { Node, NodeDef, NodeInitializer, NodeMessage } from "node-red";
 import { LangChainConfigNodeDef } from "./langchain-config";
 
 import OpenAI from 'openai';
-import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
+import { ChatCompletionMessageParam, ChatCompletionTool } from "openai/resources/chat/completions";
+import { RunnableTools } from "openai/lib/RunnableFunction";
 
+import Ajv from "ajv";
 
-import { promisify } from 'util';
+import { promisify, inspect } from 'util';
 
 export interface LangchainChatNodeDef extends NodeDef {
     name: string;
@@ -15,6 +17,14 @@ export interface LangchainChatNodeDef extends NodeDef {
     messagesType: string;
     customOptions: any;
     customOptionsType: string;
+    tools: {
+        name: string,
+        nameType: string,
+        description: string,
+        descriptionType: string,
+        schema: any,
+        schemaType: string
+    }[];
 }
 
 const LangchainChatNodeInitializer: NodeInitializer = (RED) => {
@@ -26,8 +36,8 @@ const LangchainChatNodeInitializer: NodeInitializer = (RED) => {
 
         const node = this;
 
-        node.on("input", async (msg: NodeMessage, send: any, done: any) => {
-            const { url, apiKey } = this.config;
+        node.on("input", async (msg: NodeMessage & { [k: string]: any }, send: any, done: any) => {
+            const { url, apiKey } = node.config;
             const customOptions = await evaluateNodeProperty(n.customOptions, n.customOptionsType, node, msg);
 
             const client = new OpenAI({
@@ -36,19 +46,102 @@ const LangchainChatNodeInitializer: NodeInitializer = (RED) => {
                 ...customOptions
             });
 
-            const messages = await evaluateNodeProperty(n.messages, n.messagesType, node, msg);
+            const messages: ChatCompletionMessageParam[] = await evaluateNodeProperty(n.messages, n.messagesType, node, msg);
 
-            try {
-                const chatCompletion = await client.chat.completions.create({
+            if (n.tools && n.tools.length > 0) {
+                const tools: RunnableTools<any> = await Promise.all(n.tools.map(async (it) => {
+                    const url = await evaluateNodeProperty(it.name, it.nameType, node, msg) as string;
+                    const segs = url.split('/');
+                    const name = segs[segs.length - 1];
+                    const description = await evaluateNodeProperty(it.description, it.descriptionType, node, msg);
+                    const schema = await evaluateNodeProperty(it.schema, it.schemaType, node, msg);
+                    return {
+                        type: 'function',
+                        function: {
+                            parse: (args: any) => {
+                                const data = JSON.parse(args);
+                                const ajv = new Ajv();
+                                const valid = ajv.validate(schema, data);
+                                if (!valid) {
+                                    const error = new Error(ajv.errors!.map(it => `${it}`).join('\n'))
+                                    error.name = 'ParsedError'
+                                    throw error;
+                                }
+                                return data;
+                            },
+                            function: async (args: any) => {
+                                if (name === '__structured_output__') {
+                                    return args;
+                                }
+                                try {
+                                    const response = await fetch(url, { // TODO(QL): Custom headers, query?
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify(args)
+                                    })
+                                    const text = await response.text();
+                                    try {
+                                        const json = JSON.parse(text);
+                                        return json;
+                                    } catch (error) {
+                                        return text;
+                                    }
+                                } catch (error) {
+                                    return error;
+                                }
+                            },
+                            name,
+                            description,
+                            parameters: schema,
+                        },
+                    }
+                }));
+
+                node.log(`runTool: ${inspect({
                     model: n.model,
                     messages,
-                });
+                    tools,
+                }, false, 10)})`);
 
-                send(chatCompletion.choices[0].message);
-                done();
-            } catch (error) {
-                node.error(`error ${error}`);
-                done(error);
+                try {
+                    const messages1: ChatCompletionMessageParam[] = [];
+                    const runner = client.beta.chat.completions
+                        .runTools({
+                            model: n.model,
+                            messages,
+                            tools,
+                        })
+                        .on('message', (message) => {
+                            node.log(`message: ${message}`);
+                            messages1.push(message);
+                        });
+
+                    const finalContent = await runner.finalContent();
+
+                    msg.payload = finalContent;
+                    msg.messages = messages1;
+                    send(msg);
+                    done();
+                } catch (error) {
+                    node.error(`error ${error}`);
+                    done(error);
+                }
+            } else {
+                try {
+                    const chatCompletion = await client.chat.completions.create({
+                        model: n.model,
+                        messages,
+                    });
+                    msg.payload = chatCompletion.choices[0].message;
+                    msg.messages = [...messages, messages];
+                    send(msg);
+                    done();
+                } catch (error) {
+                    node.error(`error ${error}`);
+                    done(error);
+                }
             }
         });
     }
